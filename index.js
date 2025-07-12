5b5798007bfcf2e67da58f018f24cfa93ea602e8
chauhan-ritik99 <critik.2021@gmail.com>
2025-07-12 10:15:37 +0530

    hi

---
+++ index.js
@@ -0,0 +1,10 @@
+setInterval(() => {
+    myfun()
+}, 1000);
+
+function myfun(){
+    let date=new Date()
+    let time=date.toLocaleTimeString()
+    document.getElementById("time").innerHTML=time
+}
+myfun()
